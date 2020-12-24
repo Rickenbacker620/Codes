3da935dd0530968bc551af06d49f202321169d82
Rikkenbacker620 <56065153+Rickenbacker620@users.noreply.github.com>
2020-12-24 12:57:22 +0800

    verilog  rename

--- Verilog/Exp8/bcd_convert.v
+++ Verilog/Exp8/bcd_convert.v
@@ -1,5 +1,5 @@
-module bcdchange(hex,bcd);
-    input [15:0] hex;
+module convert(adc,bcd);
+    input [15:0] adc;
     output [15:0] bcd;
     reg [15:0] t1;
     reg [15:0] t2;
@@ -7,15 +7,15 @@ module bcdchange(hex,bcd);
     reg [15:0] t4;
     always
     begin
-     t1=hex/128;
+     t1=adc/128;
      t2<=t/10;
      t3[15:12]=t2[3:0];
      t2=t%10;
      t3[11:8]=t2[3:0];
 
-     t4[7:0]<=hex[7:0]/10;
+     t4[7:0]<=adc[7:0]/10;
      t3[7:4]=t4[3:0];
-     t4=hex[7:0]%10;
+     t4=adc[7:0]%10;
      t3[3:0]=t4[3:0];
     end
     assign bcd=t3;
--- Verilog/Exp8/main.v
+++ Verilog/Exp8/main.v
@@ -4,9 +4,9 @@ module temp(clk, sda,scl,out_led,arrange);
     output scl;
     output [7:0] out_led;
     output [7:0] arrange;
-    wire [15:0] parse;
+    wire [15:0] adc;
     wire [15:0] in_num;
-    temp_sensor ts(clk, 1, scl, sda, parse[15:8], parse[7:0]);
-    convert bc(parse, in_num);
+    temp_sensor ts(clk, 1, scl, sda, adc[15:8], adc[7:0]);
+    convert bc(adc, in_num);
     num_top nt(clk,in_num,out_led,arrange);
 endmodule
