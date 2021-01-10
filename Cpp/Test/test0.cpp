de70f2ea5f170bbceee2e6dec06e3dbeae4fb5a7
Rikkenbacker620 <56065153+Rickenbacker620@users.noreply.github.com>
2021-01-10 17:05:04 +0800

    changename

--- Cpp/Test/test0.cpp
+++ Cpp/Test/test0.cpp
@@ -5,52 +5,45 @@
 
 using namespace std;
 
-// struct TNode
-// {
-//     TNode(int data)
-//     {
-//         this->data = data;
-//     }
-//     char data;
-//     TNode *lchild;
-//     TNode *rchild;
-// };
+struct TNode
+{
+    TNode(int data)
+    {
+        this->data = data;
+    }
+    char data;
+    TNode *lchild;
+    TNode *rchild;
+};
 
-// class BiTree
-// {
-//   public:
-//     BiTree()
-//     {
-//         CreateTree(root);
-//     }
-//     TNode *CreateTree(TNode *&root)
-//     {
-//         char ch;
-//         cin >> ch;
-//         if (ch == '#')
-//             root == nullptr;
-//         else
-//         {
-//             root = new TNode(ch);
-//             CreateTree(root->lchild);
-//             CreateTree(root->rchild);
-//         }
-//         return root;
-//     }
+class BiTree
+{
+  public:
+    BiTree()
+    {
+        CreateTree(root);
+    }
+    TNode *CreateTree(TNode *&root)
+    {
+        char ch;
+        cin >> ch;
+        if (ch == '#')
+            root == nullptr;
+        else
+        {
+            root = new TNode(ch);
+            CreateTree(root->lchild);
+            CreateTree(root->rchild);
+        }
+        return root;
+    }
 
-//   private:
-//     TNode *root;
-// };
+  private:
+    TNode *root;
+};
 
 int
 main()
 {
-    // BiTree t;
-    // int a;
-    // int b;
-    int a[]{1, 3, 5, 7, 9};
-    for (int i = 0; i < 5; i++)
-    {
-        cout << a[i] << endl;
-    }
+    BiTree t;
 }
